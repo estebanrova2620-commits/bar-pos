b07473b803d5954ee02268a2e7030befc85c6d73
estebanrova2620-commits <estebanrova2620@gmail.com>
2026-05-15 20:49:46 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,3 @@ import ReactDOM from "react-dom/client";
 import BarApp from "./BarApp";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><BarApp /></React.StrictMode>);
-
